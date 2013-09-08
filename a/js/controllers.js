c1a40aca3e68ce0b3befda2bb767a9b0c0374654
samuelchase <sam.chase@gmail.com>
2013-09-08 08:08:13 -0700

    sam

--- a/js/controllers.js
+++ a/js/controllers.js
@@ -145,7 +145,8 @@ function musicCtrl($scope, $http) {
     console.log(songname)
     $scope.tag_object.radiokey = token
     $scope.tag_object.songname = songname
-    $http.post('/addrdiotag', $scope.results).success(function(data){
+    $scope.tag_object.song_data = $scope.results
+    $http.post('/addrdiotag', $scope.tag_object.song_data).success(function(data){
       console.log(data)
       $scope.tag_object.tagname = ''
       $scope.tagit = false
