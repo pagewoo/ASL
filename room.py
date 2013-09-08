6ec00d8eae54f4694b128236044d44128354e34d
samuelchase <sam.chase@gmail.com>
2013-09-07 22:12:36 -0700

    added stuff

--- room.py
+++ room.py
@@ -11,6 +11,7 @@ from google.appengine.api import users
 from google.appengine.ext import db
 from models import *
 import uuid
+from api.crunchbase import Crunchbase
 
 class EnterRoom(webapp2.RequestHandler):
 	def get(self, room_name):
@@ -41,23 +42,28 @@ class EnterRoom(webapp2.RequestHandler):
 class SendMessage(webapp2.RequestHandler):
 	def post(self):
 		data = json.loads(self.request.body)
+		send_message(data)
+		logging.error('MESSAGE ' + str(data['message']))
+		messsage_scan = messsage_scan(data['message'])
+		self.response.out.write(json.dumps(messsage_scan))
 
-		message = data['message']
-		room_name = data['room_name']
-		logging.info('room ' + room_name)
-		user = users.get_current_user()
 
-		if not room_name:
-			logging.error("NO ROOM NAME")
+def send_message(self):
+	message = data['message']
+	room_name = data['room_name']
+	logging.info('room ' + room_name)
+	user = users.get_current_user()
 
-		users_in_room = Room.gql('where room_name = :1', room_name).fetch(100)
-		if not users_in_room:
-			logging.error("ROOM NOT FOUND")
+	if not room_name:
+		logging.error("NO ROOM NAME")
 
-		for room_user in users_in_room:
-			channel.send_message(room_user.user_id + '_' + room_user.room_name, json.dumps(data['message']))
+	users_in_room = Room.gql('where room_name = :1', room_name).fetch(100)
+	if not users_in_room:
+		logging.error("ROOM NOT FOUND")
 
-		self.response.out.write('complete')
+	for room_user in users_in_room:
+		channel.send_message(room_user.user_id + '_' + room_user.room_name, json.dumps(data['message']))
+
 
 
 def hyphenate(lt):
@@ -78,42 +84,88 @@ def send_to_room(hashtag, message):
 		logging.warning('No Room Found')
 		return False
 
+	data['message'] = message
+	data['room_name'] = room.room_name
 	send_message(message)
+	return True
 
 
-def messsage_scan(message):
-	symbols = [ '#' : 'topic',
-				'@' :'user',
-				'!' : 'music', 
-				'^' : 'lookup',
-			    '*' : 'calendar']
 
+def hashtags(message):
 	p = re.compile(r'#+')
 	matches = p.findall(message)
 	if matches:
 		for match in matches:
 			# add to topic list
-			send_to_room(match)
+			sent = send_to_room(match)
+			if sent:
+				logging.info('MESSAGES SENT')
+				return True
+			else:
+				logging.error('SEND FAILS')
+				return False
 
+def mentions(message):
 	p = re.compile(r'@+')
 	matches =p.findall(message)
-	if matches:
-		# add to user stream
+	for match in matches:
+		username = match.strip('@')
+		# add to user last mention
+		user = User.gql('where username == :1', username).get()
+		if not user:
+			logging.warning("NO USER FOUND")
+		else:
+			user.last_mention = message
+			user.put()
+			logging.info("Last Message saved")
 
+
+def play_song(message):
 	p = re.compile(r'!+')
 	matches =p.findall(message)
 	if matches:
 		# play song
 
+
+def crunch_base(message):
+	crunch_results = []
 	p = re.compile(r'\^+')
 	matches =p.findall(message)
-	if matches:
+	for match in matches:
+		query = match.strip('^')
+		crunchbase = CrunchBase()
 		# lookup with crunchbase
+		crunch = crunchbase.company_summary(query)
+		crunch_results.append(crunch)
 
-	p = re.compile(r'\*+')
-	matches =p.findall(message)
-	if matches:
-		# add to calendar
+	return crunch_results
+
+# def add_to_calendar(message):
+# 	p = re.compile(r'\*+')
+# 	matches =p.findall(message)
+# 	if matches:
+# 		# add to calendar
+
+def messsage_scan(message):
+	tag_success = None
+	mention_success = None
+	crunch_results = None
+	song_success = None
+
+	tag_success = hashtags(message)
+	# mention_success = mentions(message)
+	# crunch_results = crunch_base(message)
+	# song_success = play_song(message)
+
+	results = {'tag_success' : tag_success,
+			   'mention_success' : mention_success,
+			   'crunch_results' : crunch_results
+			   'song_success' : song_success}
+
+	return results
+
+			
+	
 
 
 
