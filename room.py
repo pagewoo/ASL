ec553cd63d794aba6bbecf9d228b9a39ecb0fb06
Jason Crilly <jason@pagewoo.com>
2013-09-08 02:08:46 -0700

    latest

--- room.py
+++ room.py
@@ -62,17 +62,18 @@ class SaveProfile(webapp2.RequestHandler):
 		user.username = asl['username']
 		user.age = int(asl['age'])
 		user.sex = asl['sex']
-		user.location = asl['location']
+		user.loc = asl['loc']
 		user.put()
 
 
 def create_or_get_user(user_id):
 
-	user_obj = Users.gql('where name = :1', user_id).get()
+	user_obj = Users.gql('WHERE user_id = :1', user_id).get()
 	if not user_obj:
 		user_obj = Users()
 		if users.get_current_user():
 			user_obj.username = users.get_current_user().nickname()
+			user_obj.real_user = True
 		else:
 			user_obj.username = user_id
 		user_obj.user_id = user_id
@@ -189,11 +190,11 @@ def hashtags(message):
 
 def mentions(message):
 	p = re.compile(r'@\w+')
-	matches =p.findall(message)
+	matches = p.findall(message['message'])
 	for match in matches:
 		username = match.replace('@','')
 		# add to user last mention
-		user = User.gql('where username = :1', username).get()
+		user = Users.gql('where username = :1', username).get()
 		if not user:
 			logging.warning("NO USER FOUND")
 		else:
